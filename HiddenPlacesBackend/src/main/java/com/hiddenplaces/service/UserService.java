41f16c7b6a7af70bccbb3a0c7f2fc63034580e4a
vaishnavijagtap3 <vaishnavi.jagtap.cmau25@gmail.com>
2025-12-02 22:08:41 +0530

    Update UserService.java

--- HiddenPlacesBackend/src/main/java/com/hiddenplaces/service/UserService.java
+++ HiddenPlacesBackend/src/main/java/com/hiddenplaces/service/UserService.java
@@ -1,15 +1,26 @@
 package com.hiddenplaces.service;
 
-
+import java.util.List;
 import com.hiddenplaces.dto.LoginRequest;
 import com.hiddenplaces.dto.RegisterRequest;
 import com.hiddenplaces.entity.User;
+import com.hiddenplaces.dto.ViewerDTO;
+import com.hiddenplaces.entity.Role;
+
 
 public interface UserService {
     
     User loginUser(LoginRequest loginRequest);
     
     User registerUser(RegisterRequest registerRequest);
+
+    List<ViewerDTO> getAllViewers();
+	
+	public User getUserById(Long userId);
+	
+	String  updateViewer(Long userId,ViewerDTO dto,Role role);
+	
+	String deleteDetails(Long userId,Role role);
     
 
 }
